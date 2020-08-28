b294bcbd2896754e46eda799fc1abf65a3c4783b
AgataLub <agat0538@stud.kea.dk>
2020-08-28 13:16:29 +0200

    commit 2

--- script.js
+++ script.js
@@ -6,4 +6,31 @@ fetch("https://kea-alt-del.dk/t5/api/productlist")
 
 .then(function (data) {
     console.log(data)
+    dataReceived(data);
 })
+
+
+function dataReceived(product) {
+    //loop through products
+    product.forEach(showProduct)
+}
+
+
+//executed once for each product
+function showProduct(myProduct){
+    console.log(myProduct)
+    //finding the template
+    const temp = document.querySelector("#productTemplate").content;
+    //clone the template
+    const myCopy = temp.cloneNode(true);
+    //fill out the template
+    myCopy.querySelector("h1").textContent = myProduct.name;
+    myCopy.querySelector(".picture").src = myProduct.image;
+    myCopy.querySelector(".price").textContent = myProduct.price;
+    myCopy.querySelector(".discount").textContent = myProduct.discount;
+    myCopy.querySelector(".vege").textContent = myProduct.vegetarian;
+    myCopy.querySelector(".short").textContent = myProduct.shortdescripltion;
+    //append
+    const parentElem = document.querySelector("section#starter");
+    parentElem.appendChild(myCopy)
+}
